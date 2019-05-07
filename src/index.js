279dc6e1e5e54a50f751d1d955a244168c07c097
Дмитрий Бубненков <bubnenkov@fas.gov.ru>
2019-05-07 15:17:43 +0300

    minor improvement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
+import './components/assets/element-variables.scss'
 import Vue from 'vue';
 import App from './App.vue';
 import ElementUI from 'element-ui'
+import Router from 'vue-router'
 
 // change this for localization
 //import locale from 'element-ui/lib/locale/lang/ru-RU'
 //Vue.use(ElementUI, { locale })
 
 window.axios = require('axios');
-
-import './components/assets/element-variables.scss'
+// globalThis.base_url = "http://127.0.0.1:8000"
 
 Vue.use(ElementUI);
+Vue.use(Router)
 
 new Vue({
   el: '#app',
+  // locale: 'ru',
   render: h => h(App)
 });
